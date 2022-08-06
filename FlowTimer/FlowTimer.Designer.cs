d992e0e525e01dd91681c2bc48748117af5f921a
Michael Cullen <michaelcullen@live.co.za>
2022-08-06 21:24:59 +0900

    Updated the UI and removed broken / unfinished features

---
+++ FlowTimer/FlowTimer.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace FlowTimer
+{
+    partial class FlowTimer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlowTimer));
+      this.lblTimeHeading = new System.Windows.Forms.Label();
+      this.lblBreak = new System.Windows.Forms.Label();
+      this.tbxBreak = new System.Windows.Forms.TextBox();
+      this.tbxTimeEnlapsed = new System.Windows.Forms.TextBox();
+      this.tmrWork = new System.Windows.Forms.Timer(this.components);
+      this.btnStart = new System.Windows.Forms.Button();
+      this.btnPause = new System.Windows.Forms.Button();
+      this.btnBreak = new System.Windows.Forms.Button();
+      this.btnReset = new System.Windows.Forms.Button();
+      this.tmrBreak = new System.Windows.Forms.Timer(this.components);
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.showBreakTimerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.menuStrip1.SuspendLayout();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // lblTimeHeading
+      // 
+      this.lblTimeHeading.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this.lblTimeHeading.AutoSize = true;
+      this.lblTimeHeading.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.lblTimeHeading.Location = new System.Drawing.Point(31, 10);
+      this.lblTimeHeading.Name = "lblTimeHeading";
+      this.lblTimeHeading.Size = new System.Drawing.Size(96, 22);
+      this.lblTimeHeading.TabIndex = 0;
+      this.lblTimeHeading.Text = "Work Time:";
+      // 
+      // lblBreak
+      // 
+      this.lblBreak.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this.lblBreak.AutoSize = true;
+      this.lblBreak.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.lblBreak.Location = new System.Drawing.Point(29, 54);
+      this.lblBreak.Name = "lblBreak";
+      this.lblBreak.Size = new System.Drawing.Size(99, 22);
+      this.lblBreak.TabIndex = 2;
+      this.lblBreak.Text = "Break Time:";
+      // 
+      // tbxBreak
+      // 
+      this.tbxBreak.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tbxBreak.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.tbxBreak.Location = new System.Drawing.Point(161, 46);
+      this.tbxBreak.Name = "tbxBreak";
+      this.tbxBreak.ReadOnly = true;
+      this.tbxBreak.Size = new System.Drawing.Size(153, 38);
+      this.tbxBreak.TabIndex = 2;
+      // 
+      // tbxTimeEnlapsed
+      // 
+      this.tbxTimeEnlapsed.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tbxTimeEnlapsed.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.tbxTimeEnlapsed.Location = new System.Drawing.Point(161, 3);
+      this.tbxTimeEnlapsed.Name = "tbxTimeEnlapsed";
+      this.tbxTimeEnlapsed.ReadOnly = true;
+      this.tbxTimeEnlapsed.Size = new System.Drawing.Size(153, 38);
+      this.tbxTimeEnlapsed.TabIndex = 0;
+      // 
+      // tmrWork
+      // 
+      this.tmrWork.Enabled = true;
+      this.tmrWork.Interval = 1000;
+      this.tmrWork.Tick += new System.EventHandler(this.tmrWork_Tick);
+      // 
+      // btnStart
+      // 
+      this.btnStart.BackColor = System.Drawing.SystemColors.Control;
+      this.btnStart.BackgroundImage = global::FlowTimer.Properties.Resources.imgPlay;
+      this.btnStart.FlatAppearance.BorderSize = 0;
+      this.btnStart.Location = new System.Drawing.Point(11, 119);
+      this.btnStart.Margin = new System.Windows.Forms.Padding(0);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(75, 75);
+      this.btnStart.TabIndex = 2;
+      this.btnStart.UseVisualStyleBackColor = false;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // btnPause
+      // 
+      this.btnPause.BackColor = System.Drawing.SystemColors.Control;
+      this.btnPause.Enabled = false;
+      this.btnPause.Location = new System.Drawing.Point(92, 119);
+      this.btnPause.Name = "btnPause";
+      this.btnPause.Size = new System.Drawing.Size(75, 75);
+      this.btnPause.TabIndex = 3;
+      this.btnPause.UseVisualStyleBackColor = false;
+      this.btnPause.Click += new System.EventHandler(this.btnStop_Click);
+      // 
+      // btnBreak
+      // 
+      this.btnBreak.BackColor = System.Drawing.SystemColors.Control;
+      this.btnBreak.Enabled = false;
+      this.btnBreak.Location = new System.Drawing.Point(173, 119);
+      this.btnBreak.Name = "btnBreak";
+      this.btnBreak.Size = new System.Drawing.Size(75, 75);
+      this.btnBreak.TabIndex = 4;
+      this.btnBreak.UseVisualStyleBackColor = false;
+      this.btnBreak.Click += new System.EventHandler(this.btnBreak_Click);
+      // 
+      // btnReset
+      // 
+      this.btnReset.BackColor = System.Drawing.SystemColors.Control;
+      this.btnReset.Enabled = false;
+      this.btnReset.Location = new System.Drawing.Point(254, 119);
+      this.btnReset.Name = "btnReset";
+      this.btnReset.Size = new System.Drawing.Size(75, 75);
+      this.btnReset.TabIndex = 5;
+      this.btnReset.UseVisualStyleBackColor = false;
+      this.btnReset.Click += new System.EventHandler(this.btnFinish_Click);
+      // 
+      // tmrBreak
+      // 
+      this.tmrBreak.Interval = 1000;
+      this.tmrBreak.Tick += new System.EventHandler(this.tmrBreak_Tick);
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem1});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(340, 26);
+      this.menuStrip1.TabIndex = 6;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.Checked = true;
+      this.fileToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showBreakTimerToolStripMenuItem,
+            this.alwaysOnTopToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(71, 22);
+      this.fileToolStripMenuItem.Text = "Settings";
+      // 
+      // showBreakTimerToolStripMenuItem
+      // 
+      this.showBreakTimerToolStripMenuItem.Checked = true;
+      this.showBreakTimerToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.showBreakTimerToolStripMenuItem.Name = "showBreakTimerToolStripMenuItem";
+      this.showBreakTimerToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+      this.showBreakTimerToolStripMenuItem.Text = "Show Break Timer";
+      this.showBreakTimerToolStripMenuItem.CheckedChanged += new System.EventHandler(this.chbxShowBreak_CheckedChanged);
+      this.showBreakTimerToolStripMenuItem.Click += new System.EventHandler(this.showBreakTimerToolStripMenuItem_Click);
+      // 
+      // alwaysOnTopToolStripMenuItem
+      // 
+      this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+      this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+      this.alwaysOnTopToolStripMenuItem.Text = "Always On Top";
+      this.alwaysOnTopToolStripMenuItem.CheckedChanged += new System.EventHandler(this.alwaysOnTopToolStripMenuItem_CheckedChanged);
+      // 
+      // helpToolStripMenuItem
+      // 
+      this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+      this.helpToolStripMenuItem.Size = new System.Drawing.Size(51, 22);
+      this.helpToolStripMenuItem.Text = "Help";
+      this.helpToolStripMenuItem.Click += new System.EventHandler(this.rulesToolStripMenuItem_Click);
+      // 
+      // aboutToolStripMenuItem1
+      // 
+      this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+      this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(60, 22);
+      this.aboutToolStripMenuItem1.Text = "About";
+      this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.rulesToolStripMenuItem_Click);
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 2;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.Controls.Add(this.lblTimeHeading, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.tbxBreak, 1, 1);
+      this.tableLayoutPanel1.Controls.Add(this.lblBreak, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.tbxTimeEnlapsed, 1, 0);
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 29);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 2;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(317, 87);
+      this.tableLayoutPanel1.TabIndex = 7;
+      // 
+      // FlowTimer
+      // 
+      this.AcceptButton = this.btnStart;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(340, 204);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Controls.Add(this.btnReset);
+      this.Controls.Add(this.btnBreak);
+      this.Controls.Add(this.btnPause);
+      this.Controls.Add(this.btnStart);
+      this.Controls.Add(this.menuStrip1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "FlowTimer";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "FlowTimer";
+      this.Load += new System.EventHandler(this.Form_Load);
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTimeHeading;
+        private System.Windows.Forms.Label lblBreak;
+        private System.Windows.Forms.TextBox tbxBreak;
+        private System.Windows.Forms.TextBox tbxTimeEnlapsed;
+        private System.Windows.Forms.Timer tmrWork;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnBreak;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer tmrBreak;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem showBreakTimerToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+  }
+}
+
---
+++ FlowTimer/FlowTimer.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace FlowTimer {
+  public partial class FlowTimer : Form {
+    public Stopwatch enlapsedTime = new Stopwatch();
+
+    string soundStartLoc;
+    string soundEndLoc;
+    string soundBreakLoc;
+    string soundPauseLoc;
+    SoundPlayer soundStart = new SoundPlayer();
+    SoundPlayer soundBreakFin = new SoundPlayer();
+    SoundPlayer soundBreak = new SoundPlayer();
+    SoundPlayer soundPause = new SoundPlayer();
+
+    public void Form_Load(object sender, EventArgs e) {
+      tbxTimeEnlapsed.Text = "00:00:00";
+      tbxBreak.Text = "00:00:00";
+
+      this.Icon = Icon.ExtractAssociatedIcon("graphics/icon.ico");
+
+      soundStartLoc = $"sounds/bing.wav";
+      soundBreakLoc = $"sounds/bong.wav";
+      soundEndLoc = $"sounds/break_fin.wav";
+      soundPauseLoc = $"sounds/bong_short.wav";
+
+      btnStart.BackgroundImage = Image.FromFile($"graphics/imgPlay.png");
+      btnStart.BackgroundImageLayout = ImageLayout.Zoom;
+      btnPause.BackgroundImage = Image.FromFile($"graphics/imgPause.png");
+      btnPause.BackgroundImageLayout = ImageLayout.Zoom;
+      btnBreak.BackgroundImage = Image.FromFile($"graphics/imgStop.png");
+      btnBreak.BackgroundImageLayout = ImageLayout.Zoom;
+      btnReset.BackgroundImage = Image.FromFile($"graphics/imgReset.png");
+      btnReset.BackgroundImageLayout = ImageLayout.Zoom;
+    }
+
+    #region BUTTONS
+    private void btnStart_Click(object sender, EventArgs e) {
+      enlapsedTime.Start();
+
+      btnStart.Enabled = false;
+      btnPause.Enabled = true;
+      btnBreak.Enabled = true;
+      btnReset.Enabled = true;
+
+      soundStart.SoundLocation = soundStartLoc;
+      soundStart.Play();
+
+    }
+
+    private void btnStop_Click(object sender, EventArgs e) //pause button
+    {
+      enlapsedTime.Stop();
+
+      btnStart.Enabled = true;
+      btnPause.Enabled = false;
+
+      soundPause.SoundLocation = soundPauseLoc;
+      soundPause.Play();
+
+    }
+
+    private void btnBreak_Click(object sender, EventArgs e) {
+      DialogResult x = MessageBox.Show("Are you sure you want to begin your break?", "", MessageBoxButtons.YesNo);
+      enlapsedTime.Stop();
+
+      if (x == DialogResult.Yes) {
+        TimeSpan workTime = enlapsedTime.Elapsed;
+        double breakSeconds = (enlapsedTime.ElapsedMilliseconds / 1000) * 0.16666666666666667;
+        TimeSpan breakTime = TimeSpan.FromSeconds(breakSeconds);
+
+        soundBreak.SoundLocation = soundBreakLoc;
+        soundBreak.Play();
+
+        tmrBreak.Enabled = true;
+        tmrBreak.Start();
+
+        tmrWork.Stop();
+      }
+      else {
+        enlapsedTime.Start();
+      }
+
+
+    }
+
+    private void btnFinish_Click(object sender, EventArgs e) //reset button
+    {
+      DialogResult x = MessageBox.Show("Are you sure you want to reset the timer?", "Reset", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+      if (x == DialogResult.Yes) {
+        ResetTimer();
+      }
+    }
+    #endregion
+
+    #region TIMERS
+    public void tmrWork_Tick(object sender, EventArgs e) {
+      double breakSeconds = (enlapsedTime.ElapsedMilliseconds / 1000) * 0.222222222222222;
+
+      TimeSpan breakSpan = TimeSpan.FromSeconds(breakSeconds);
+
+      TimeSpan ts = enlapsedTime.Elapsed;
+
+      #region MAIN_TIMER
+      if (ts.Seconds < 10 & ts.Minutes < 10)  //main timer
+      {
+        tbxTimeEnlapsed.Text = $"0{ts.Hours}:0{ts.Minutes}:0{ts.Seconds}";
+      }
+      else if (ts.Seconds < 10 & ts.Minutes >= 10) {
+        tbxTimeEnlapsed.Text = $"0{ts.Hours}:{ts.Minutes}:0{ts.Seconds}";
+      }
+      else if (ts.Seconds >= 10 & ts.Minutes < 10) {
+        tbxTimeEnlapsed.Text = $"0{ts.Hours}:0{ts.Minutes}:{ts.Seconds}";
+      }
+      else {
+        tbxTimeEnlapsed.Text = $"0{ts.Hours}:{ts.Minutes}:{ts.Seconds}";
+      }
+      #endregion
+
+      #region BREAK_TIMER
+      if (btnPause.Enabled)   //break timer
+      {
+        if (breakSpan.Seconds < 10 & breakSpan.Minutes < 10) {
+          tbxBreak.Text = $"00:0{breakSpan.Minutes}:0{breakSpan.Seconds}";
+        }
+        else if (breakSpan.Seconds >= 10 & breakSpan.Minutes < 10) {
+          tbxBreak.Text = $"00:0{breakSpan.Minutes}:{breakSpan.Seconds}";
+        }
+        else if (breakSpan.Seconds < 10 & breakSpan.Minutes >= 10) {
+          tbxBreak.Text = $"00:{breakSpan.Minutes}:0{breakSpan.Seconds}";
+        }
+        else if (ts.Minutes >= 90) {
+          tbxBreak.Text = "00:15:00";
+        }
+        else {
+          tbxBreak.Text = $"{breakSpan.Minutes}:{breakSpan.Seconds}";
+        }
+
+      }
+      #endregion
+    }
+
+    public void tmrBreak_Tick(object sender, EventArgs e) {
+      string breakTimeString = tbxBreak.Text;
+      TimeSpan newTime = TimeSpan.Parse($"00:{breakTimeString}");
+      double totSeconds = newTime.TotalSeconds;
+      bool endSoundPlayed = false;
+
+      if (totSeconds != 0) {
+        tbxBreak.Text = newTime.Subtract(TimeSpan.FromSeconds(1)).ToString();
+      }
+      else if (totSeconds == 0 & endSoundPlayed == false) {
+        soundBreakFin.SoundLocation = soundEndLoc;
+        soundBreakFin.Play();
+
+        tmrBreak.Stop();
+      }
+    }
+    #endregion
+
+    #region TOOLSTRIP_MENU_ITEMS
+
+    private void rulesToolStripMenuItem_Click(object sender, EventArgs e) {
+      MessageBox.Show(
+          @"This app awards you 20 minutes of break time for every 90 minutes of work.
+                This amount will scale up and down, depending on how long you work for.",
+          "How to Use",
+          MessageBoxButtons.OK,
+          MessageBoxIcon.Information
+          );
+    }
+    #endregion
+
+    #region OTHERS
+    private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e) {
+
+    }
+
+    private void chbxShowBreak_CheckedChanged(object sender, EventArgs e) {
+      if (showBreakTimerToolStripMenuItem.Checked) {
+        lblBreak.Visible = true;
+        tbxBreak.Visible = true;
+      }
+
+      if (!showBreakTimerToolStripMenuItem.Checked) {
+        lblBreak.Visible = false;
+        tbxBreak.Visible = false;
+      }
+    }
+
+    private void ResetTimer() {
+      enlapsedTime.Reset();
+      btnBreak.Enabled = false;
+      btnReset.Enabled = false;
+      btnPause.Enabled = false;
+      btnStart.Enabled = true;
+      tbxTimeEnlapsed.Text = "00:00:00";
+      tbxBreak.Text = "00:00:00";
+    }
+
+    #endregion
+
+    public FlowTimer() {
+      InitializeComponent();
+    }
+
+    private void alwaysOnTopToolStripMenuItem_CheckedChanged(object sender, EventArgs e) {
+
+    }
+
+    private void showBreakTimerToolStripMenuItem_Click(object sender, EventArgs e) {
+      if (showBreakTimerToolStripMenuItem.Checked) {
+        showBreakTimerToolStripMenuItem.Checked = false;
+      }
+      else {
+        showBreakTimerToolStripMenuItem.Checked = true;
+      }
+    }
+  }
+}
